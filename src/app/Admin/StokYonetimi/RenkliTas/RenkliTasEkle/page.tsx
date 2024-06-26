955054f6afe52d8c5cceb27f602bba33b6f6d85b
SerkanAfsar <serkan-afsar@hotmail.com>
2024-06-26 13:53:49 +0300

    Renkli Tas Sayfalari Eklendi

---
+++ src/app/Admin/StokYonetimi/RenkliTas/RenkliTasEkle/page.tsx
@@ -0,0 +1,11 @@
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+export default function RenkliTaskEkle() {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Renkli Taş Ekle" />
+      <div>Renkli Taş Ekleme Sayfası (Hazırlanıyor)</div>
+    </DefaultLayout>
+  );
+}
