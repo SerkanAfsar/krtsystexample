a446b8a38ba7b96264671c43c705a481859e3d1d
SerkanAfsar <serkan-afsar@hotmail.com>
2024-10-24 12:49:47 +0300

    Custom Page Modal Css Updated

--- src/components/CustomModals/CustomPageModal.tsx
+++ src/components/CustomModals/CustomPageModal.tsx
@@ -19,17 +19,23 @@ export default function CustomModalPage({
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="shadown-md absolute left-[50%] top-[50%] h-[90%] w-[90%] -translate-x-[50%] -translate-y-[50%] overflow-auto rounded-md bg-white p-4"
+        className="shadown-md absolute left-[50%] top-[50%]  h-[90%] w-[90%] -translate-x-[50%] -translate-y-[50%]  rounded-md  bg-white "
       >
-        <div
-          onClick={() => setModalDataValue()}
-          className="absolute right-5 top-4 z-999999 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-[#000] p-3 font-bold text-white"
-        >
-          X
+        <div className="flex h-full w-full flex-col">
+          <div className="border-b p-4 shadow-md">
+            <div
+              onClick={() => setModalDataValue()}
+              className="absolute right-5 top-4 z-999999 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-[#000] p-3 font-bold text-white"
+            >
+              X
+            </div>
+            <h1 className="block w-full  text-lg font-bold text-[#000]">
+              {title}
+            </h1>
+          </div>
+
+          <div className="m-4 block flex-1 overflow-auto p-4">{children}</div>
         </div>
-        <h1 className="block w-full text-lg font-bold text-[#000]">{title}</h1>
-        <br />
-        {children}
       </div>
     </div>
   );
